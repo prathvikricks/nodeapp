631c120a73de210c661655244741322906ce3eb3
prathvikricks <71117919+prathvikricks@users.noreply.github.com>
2024-10-14 16:21:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({ message: "Hello from docker 🐳"});
+})
+
+app.listen(9000, ()=> console.log("Server is running on port 9000"));
